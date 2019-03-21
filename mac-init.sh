2e2b35dd05141988bd7e3d2bf340eccad22f490d
Jimmy Yin <jimmy.yin5@gmail.com>
2019-03-21 11:15:37 +0800

    Create mac-init.sh

---
+++ mac-init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# 2019.03.21
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install bash-completion
+brew install wget
+brew install git
+brew install privoxy
+brew install mysql@5.7
+brew cask install typora
