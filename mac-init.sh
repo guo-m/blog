32da8126a55281b686712007eeb73c4a8abffcea
Jimmy Yin <jimmy.yin5@gmail.com>
2019-03-22 10:14:26 +0800

    Update mac-init.sh

--- mac-init.sh
+++ mac-init.sh
@@ -4,7 +4,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install bash-completion
-brew install wget
+brew install wget imagemagick
 brew install git
 brew install privoxy
 brew install mysql@5.7
